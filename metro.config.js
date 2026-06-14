c2986803802591bae6daa3cc8709a3559aa722e5
Toughson <brighttoughson@gmail.com>
2026-06-14 05:22:28 +0000

    fix: remove unstable_workerThreads warning from metro config

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,9 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts.push('mjs');
 
+// Delete the property that causes the Expo validation warning
+if (config.watcher && 'unstable_workerThreads' in config.watcher) {
+  delete config.watcher.unstable_workerThreads;
+}
+
 module.exports = config;
